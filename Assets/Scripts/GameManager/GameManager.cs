7fcb539850d6f293758bd5b79b1f2ba2ada2e515
TranQuangDuy <duy.tran@beesightsoft.com>
2017-12-15 18:10:25 +0700

    add options menu

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+	public Dropdown resDropdown;
+	private Resolution[] resolutions;
 
 	private int quantityAlive;
 	private int quantityDead;
@@ -18,6 +20,21 @@ public class GameManager : MonoBehaviour
 		quantityDeadBoard = GameObject.FindGameObjectWithTag ("QuantityDead").GetComponent<Text> ();
 		quantityAliveBoard.text = "0";
 		quantityDeadBoard.text = "0";
+
+		int currentResIndex = 0;
+		resolutions = Screen.resolutions;
+		resDropdown.ClearOptions ();
+		List<string> options = new List<string> ();
+		for (int i = 0; i < resolutions.Length; i++) {
+			string option = resolutions [i].width + " x " + resolutions [i].height;
+			options.Add (option);
+			if (resolutions [i].width == Screen.currentResolution.width && resolutions [i].height == Screen.currentResolution.height) {
+				currentResIndex = i;
+			}
+		}
+		resDropdown.AddOptions (options);
+		resDropdown.value = currentResIndex;
+		resDropdown.RefreshShownValue ();
 	}
 	
 	// Update is called once per frame
@@ -32,7 +49,33 @@ public class GameManager : MonoBehaviour
 		quantityDead++;
 		quantityDeadBoard.text = quantityDead.ToString ();
 		if (quantityDead >= 5) {
-			FindObjectOfType<LevelLoader> ().LoadLevel ("Level2");
+			FindObjectOfType<LoadingManager> ().LoadLevel ("Level2");
 		}
 	}
+
+	public void PauseGame (bool isPause)
+	{
+		if (isPause) {
+			Time.timeScale = 0;
+		} else {
+			Time.timeScale = 1;
+		}
+	}
+
+	public void SetQuality (int qualityIndex)
+	{
+		QualitySettings.SetQualityLevel (qualityIndex);
+	}
+
+	public void SetFullScreen (bool isFullScreen)
+	{
+		Screen.fullScreen = isFullScreen;
+	}
+
+	public void SetResolution (int resIndex)
+	{
+		Resolution res = resolutions [resIndex];
+		Screen.SetResolution (res.width, res.height, Screen.fullScreen);
+	}
+
 }
